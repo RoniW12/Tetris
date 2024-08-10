526ce3cda89f5ab242a13832151f673da7b37d1b
mayad <mayad@MSI.home>
2024-08-11 00:19:06 +0300

    Fixed bug with rotation and added L,R
    Fixed issue with pass by reference that I believe was causing issues
    with rotation. Also added rotation functionality to L and R shapes

--- Tetris/src/tetris/Pieces_Library.java
+++ Tetris/src/tetris/Pieces_Library.java
@@ -2,7 +2,6 @@ package tetris;
 
 
 class T_Shape extends Piece{
-	int orientation = 0;
 	private static final int[][][] SHAPES = {
 			{{-2,-2,-2},{-2,-1,-2},{-1,-1,-1}},
 			{{-1,-2,-2},{-1,-1,-2},{-1,-2,-2}},
@@ -36,7 +35,7 @@ class T_Shape extends Piece{
 		
 		orientation = (orientation + 1)%4;
 
-		if(pos.get(0) >= 50) {
+		if(pos.get(0)+3 > 49 || super.Check_Collision()) {
 			return;
 		}
 		board.thread_lock.writeLock().lock();
@@ -59,9 +58,14 @@ class T_Shape extends Piece{
 }
 
 class r_Shape extends Piece{
-	private static final int[][] INITIAL_SHAPE = {
-	        {-1, -1, -2}, {-1, -2, -2}, {-1, -2, -2}
-	    };
+	private static final int[][][] SHAPES = {
+			{{-2, -1, -1}, {-2, -1, -2}, {-2, -1, -2}},
+			{{-2, -2, -2}, {-1, -1, -1}, {-2, -2, -1}},
+			{{-2, -1, -2}, {-2, -1, -2}, {-1, -1, -2}},
+			{{-1, -2, -2}, {-1, -1, -1}, {-2, -2, -2}}
+			
+	};
+	private static final int[][] INITIAL_SHAPE = SHAPES[0];
 	public r_Shape() {
 		
 		super.Set_Shape(INITIAL_SHAPE);
@@ -70,13 +74,55 @@ class r_Shape extends Piece{
 	@Override
 	public void Rotate() {
 		super.Rotate();
+		
+		switch(orientation) {
+
+		case 0:
+			pos.set(1, pos.get(1)-1);
+			break;
+		
+		case 1:
+			pos.set(0, pos.get(0)-1);
+			
+			if(pos.get(0) < 0) {
+				return;
+			}
+			break;
+		}
+		
+		orientation = (orientation + 1)%4;
+		
+		if(pos.get(0)+3 > 49 || super.Check_Collision()) {
+			return;
+		}
+		board.thread_lock.writeLock().lock();
+		
+		
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				System.out.println("(" + i + ", " + j + ")" + " + " + pos.toString());
+				if(board.board[i+pos.get(0)][j+pos.get(1)] >= 0) {
+					board.board[i+pos.get(0)][j+pos.get(1)] = SHAPES[orientation][i][j]+2;
+				}
+				else {
+					board.board[i+pos.get(0)][j+pos.get(1)] = SHAPES[orientation][i][j];
+				}
+			}
+		}
+		board.thread_lock.writeLock().unlock();
+
 	}
 }
 
 class L_Shape extends Piece{
-	private static final int[][] INITIAL_SHAPE = {
-	        {-1, -2, -2}, {-1, -2, -2}, {-1, -1, -2}
-	    };
+	private static final int[][][] SHAPES = {
+			{{-1, -1, -2}, {-2, -1, -2}, {-2, -1, -2}},
+			{{-2, -2, -1}, {-1, -1, -1}, {-2, -2, -2}},
+			{{-2, -1, -2}, {-2, -1, -2}, {-2, -1, -1}},
+			{{-2, -2, -2}, {-1, -1, -1}, {-1, -2, -2}}
+			
+	};
+	private static final int[][] INITIAL_SHAPE = SHAPES[0];
 	public L_Shape() {
 		
 		super.Set_Shape(INITIAL_SHAPE);
@@ -85,8 +131,46 @@ class L_Shape extends Piece{
 	@Override
 	public void Rotate() {
 		super.Rotate();
+		
+		switch(orientation) {
+
+		case 2:
+			pos.set(1, pos.get(1)-1);
+			break;
+		
+		case 3:
+			pos.set(0, pos.get(0)-1);
+			
+			if(pos.get(0) < 0) {
+				return;
+			}
+			break;
+		}
+		
+		orientation = (orientation + 1)%4;
+		
+		if(pos.get(0)+3 > 49 || super.Check_Collision()) {
+			return;
+		}
+		board.thread_lock.writeLock().lock();
+		
+		
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				System.out.println("(" + i + ", " + j + ")" + " + " + pos.toString());
+				if(board.board[i+pos.get(0)][j+pos.get(1)] >= 0) {
+					board.board[i+pos.get(0)][j+pos.get(1)] = SHAPES[orientation][i][j]+2;
+				}
+				else {
+					board.board[i+pos.get(0)][j+pos.get(1)] = SHAPES[orientation][i][j];
+				}
+			}
+		}
+		board.thread_lock.writeLock().unlock();
+
 	}
 }
+
 
 class I_Shape extends Piece{
 	private static final int[][][] SHAPES = {
