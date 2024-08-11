ca4e1637ff57332984ac6412599ba54968d3fe0a
BinyaminAlony <binyaminalony1@gmail.com>
2024-08-11 09:46:49 +0300

    added game gui

--- Tetris/src/tetris/Board.java
+++ Tetris/src/tetris/Board.java
@@ -4,17 +4,20 @@ import java.util.Random;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+import screens.GameScreen;
+
 
 public class Board{
 	public int[][] board;
 	private Piece_Factory piece_gen;
 	public final int BOARD_SIZE_X = 10;
-	public final int BOARD_SIZE_Y = 30;
+	public final int BOARD_SIZE_Y = 20;
 	public Boolean isMoveable = true;
 	Piece current_piece;
 	final int EMPTY_ROWS = 5;
 	Random rng = new Random();
 	ReadWriteLock thread_lock = new ReentrantReadWriteLock();
+	public int score = 0;
 	
 	public Board(){
 		board = new int[BOARD_SIZE_Y][BOARD_SIZE_X];
@@ -101,6 +104,7 @@ public class Board{
 		for(int j = 0; j < BOARD_SIZE_X; j++) {
 			board[i][j] = 0;
 		}
+		score  += 10;
 
 	}
 	
--- Tetris/src/tetris/GameManager.java
+++ Tetris/src/tetris/GameManager.java
@@ -1,26 +1,90 @@
 package tetris;
 import java.util.Timer;
 import java.util.TimerTask;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import screens.GameScreen;
+import screens.TetrisOpeningScreen;
 
 
-public class GameManager  extends Thread {	
+public class GameManager{	
     TimerTask gameTask;
 	boolean isRunning = false;
-	public void run() {
-		Board board = new Board();
-		gameTask = new Helper(board);
-        KeypressSimulation test = new KeypressSimulation(board);
-        test.run();
+	private Board board;
+	private GameScreen gameScreen;
+
+	
+	public GameManager() {
+    	gameScreen = new GameScreen();
+		this.board = new Board();
+    	gameScreen.addWindowListener(new WindowAdapter() {
+      	  public void windowClosing(WindowEvent e) {
+      		  stopGame();
+    		  TetrisOpeningScreen.openScreen();
+    	  }
+    	});
+		gameTask = new Helper();
+		KeyPressHandler handler = new KeyPressHandler(board, gameScreen); 
+    	
+        JPanel panel = new JPanel();
+        panel.setOpaque(false);
+        gameScreen.add(panel);
+        // Bind actions to key strokes
+        InputMap inputMap = panel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        ActionMap actionMap = panel.getActionMap();
+        
+
+        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), "moveLeft");
+        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), "moveRight");
+        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), "moveDown");
+        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0), "spaceAction");
+        
+
+        actionMap.put("moveLeft", handler.left);
+        actionMap.put("moveRight", handler.right);
+        actionMap.put("moveDown", handler.down);
+        actionMap.put("spaceAction", handler.rotate);
+    	
+    	
+//        KeypressSimulation test = new KeypressSimulation(board);
+//        test.run();
         Timer timer = new Timer();
         isRunning = true;
         timer.schedule(gameTask, 250, 1000);
 	}
-	public boolean stopGameLoop() {
+	public boolean stopGame() {
 		if(isRunning)
 		{
 			gameTask.cancel();
+			isRunning = false;
 			return true;
 		}
 		return false;
 	}
+	
+	public class Helper extends TimerTask {
+	    public void run()
+	    {
+			board.move_down();
+//			_board.print_board();
+			updateBoard();
+	    }
+	    
+	    public void updateBoard() {
+			for(int i = 0; i < board.BOARD_SIZE_Y; i++) {
+				for(int j = 0; j< board.BOARD_SIZE_X; j++) {
+					gameScreen.setGridSquareColor(i,j,board.board[i][j]);
+				}
+			}
+			gameScreen.updateScore(board.score);
+		}
+	}	
 }
+
+
+
+
