2c812de084e9a59773f1083919359035d2819545
BinyaminAlony <binyaminalony1@gmail.com>
2024-08-13 05:18:22 +0300

    added next piece + level up flash

--- Tetris/src/tetris/GameManager.java
+++ Tetris/src/tetris/GameManager.java
@@ -23,7 +23,8 @@ public class GameManager{
     Helper gameTask;
 	boolean isRunning = false;
 	private Board board;
-	private GameScreen gameScreen;    private ScheduledExecutorService timer = Executors.newSingleThreadScheduledExecutor();
+	private GameScreen gameScreen;    
+	private ScheduledExecutorService timer = Executors.newSingleThreadScheduledExecutor();
 	private ScheduledFuture<?> scheduledFuture;
 
 	
@@ -66,7 +67,18 @@ public class GameManager{
 //        KeypressSimulation test = new KeypressSimulation(board);
 //        test.run();
         isRunning = true;
-        scheduledFuture = timer.scheduleAtFixedRate(gameTask, 250, 250, TimeUnit.MILLISECONDS); // Use scheduleAtFixedRate for fixed interval
+        System.out.println((int) (250 / board.multiplier));
+        scheduledFuture = timer.scheduleAtFixedRate(gameTask, 250, (int) (250 / board.multiplier), TimeUnit.MILLISECONDS); // Use scheduleAtFixedRate for fixed interval
+        
+//        while(isRunning) {
+//        	try {
+//        		gameTask.run();
+//				Thread.sleep((long) (250 / board.multiplier));
+//			} catch (InterruptedException e1) {
+//				// TODO Auto-generated catch block
+//				e1.printStackTrace();
+//			}
+//        }
 	}
 	public boolean stopGame(boolean openEndScreen) {
 		if (isRunning) {
@@ -97,20 +109,43 @@ public class GameManager{
 	    		System.out.println("quitting game");
 	    		stopGame(true);
 	    	}
-	    	if(counter > 20) {
+	    	if(counter > 10) {
 	    		counter = 0;
 	    		board.multiplier += 0.01;
-	    		System.out.println("********Speedup: Multiplier = "+board.multiplier+"**********");
-	            scheduledFuture = timer.schedule(this, (int) (250 / board.multiplier), TimeUnit.MILLISECONDS);
-	    	}
+	    		gameScreen.flashLevelUp();
+	    		System.out.println("********tick speed = "+(int) (50 + 200* Math.exp(-1*board.multiplier))+"**********");
+	    		scheduledFuture.cancel(true);
+	    		scheduledFuture = timer.scheduleAtFixedRate(gameTask, 250, (int) (50 + 200* Math.exp(-1*board.multiplier)), TimeUnit.MILLISECONDS);	    	}
 	    	
 			board.move_down();
 //			_board.print_board();
+			
 			updateBoard();
-			counter += 1;
+			if(board.updateNextPiece)
+			{
+				updateNextPiece();
+				board.updateNextPiece = false;
+				counter += 1;
+			}
 	    }
+
+	    private void updateNextPiece() {
+//	    	try{
+	    		int x = board.next_piece.SHAPES[0][0].length, y = board.next_piece.SHAPES[0].length;
+	    	for(int i = 0; i < 4; i++) {
+				for(int j = 0; j < 4; j++)
+				{
+					if(i < y && j < x)
+						gameScreen.setNextPieceSquareColor(i,j,board.next_piece.SHAPES[0][i][j]);
+					else
+						gameScreen.setNextPieceSquareColor(i,j,-2);
+				}
+			}
+//	    	}catch(Exception e)
+//    		{System.out.println(e.getStackTrace());}
+		}
 	    
-	    public void updateBoard() {
+		public void updateBoard() {
 			for(int i = 0; i < board.BOARD_SIZE_Y; i++) {
 				for(int j = 0; j< board.BOARD_SIZE_X; j++) {
 					gameScreen.setGridSquareColor(i,j,board.board[i][j]);
@@ -118,10 +153,6 @@ public class GameManager{
 			}
 			gameScreen.updateScore(board.score);
 		}
-	    
-	    public void finishGame(){
-	    	
-	    }
 	}	
 }
 
