b7216239a1cecdcd238e30e5b0a8b87d9231bcb2
Danielm278 <135541999+Danielm278@users.noreply.github.com>
2024-08-16 02:16:03 +0300

    Delete Tetris directory

--- Tetris/tetris/Main.java
+++
@@ -1,9 +0,0 @@
-package tetris;
-import screens.TetrisOpeningScreen;
-
-public class Main extends Thread {
-  public static void main(String[] args) {
-    TetrisOpeningScreen screenThread = new TetrisOpeningScreen();
-	screenThread.start();
-  }
-}
